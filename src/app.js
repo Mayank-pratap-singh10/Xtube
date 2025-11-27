da26f4d96aaffe4670b953635c30cb833641e941
Mayank-pratap-singh10 <mayankpratapsinghofficials@gmail.com>
2025-11-28 01:13:12 +0530

    Fix import statement for user routes in app.js

--- src/app.js
+++ src/app.js
@@ -14,8 +14,8 @@ app.use(express.json({limit: '16kb'}));
 app.use(express.urlencoded({extended: true, limit: '16kb'}));  
 
 // routes
-import { userRoute } from "./routes/user.routes";
-app.use("/api/v1/user",userRoute)
+import  userRouter from "./routes/user.routes.js";
+app.use("/api/v1/user",userRouter)
 
 
 export default app;
